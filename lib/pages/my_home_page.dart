d5ab6ef56f67dae6c4b4ee1a94f88833ffa13452
Kurosaki-Yuki <visholol2@gmail.com>
2024-04-25 19:27:01 -0400

    lab5

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/main.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+const String assetName = 'assets/icons/devrant_icon.svg';
+const String smileyName = 'assets/icons/smiley_icon.svg';
+const String sadName = 'assets/icons/sad_face_icon.svg';
+Widget svg = SvgPicture.asset(
+  assetName,
+  semanticsLabel: 'newIcon'
+);
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -20,104 +19,90 @@ class MyHomePage extends StatefulWidget {
 }
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String _victory = '';
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
+
+      _check();
     });
   }
 
   void _decrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter--;
+      if(_counter > 0){
+        _counter--;
+      }
+
+      _check();
     });
   }
 
   void _restartCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter = 0;
+
+      _check();
     });
+  }
+
+  void _check(){
+    if(_counter == 15){
+      _victory = 'Victoria';
+      svg = SvgPicture.asset(smileyName);
+    }else if(_counter == 10){
+      _victory = 'Derrota';
+      svg = SvgPicture.asset(sadName);
+    }else if(_counter < 10){
+      _victory = '';
+      svg = SvgPicture.asset(assetName);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      persistentFooterButtons: [
-        ElevatedButton(
-          onPressed: _incrementCounter,
-          child: const Icon(Icons.add),
-        ),
-        ElevatedButton(
-          onPressed: _decrementCounter,
-          child: const Icon(Icons.remove),
-        )],
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Haz apretado el boton',
-              style: TextStyle(fontSize: 100),
-            ),
-            Text(
-              '$_counter veces',
-              style: TextStyle(fontSize: 50),
-            ),
-            svg
-          ],
+        child:Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  svg,
+                  svg,
+                  svg,
+                ]
+              ),
+              Text(
+                '$_counter veces',
+                style: const TextStyle(fontSize: 30),
+              ),
+              Text(
+                _victory,
+                style: const TextStyle(fontSize: 30),
+              ),
+              const Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  TextButton(onPressed: _incrementCounter, child: Icon(Icons.add)),
+                  TextButton(onPressed: _decrementCounter, child: Icon(Icons.remove)),
+                  TextButton(onPressed: _restartCounter, child: Icon(Icons.restart_alt))
+                ]
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _restartCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.restart_alt_outlined),
-      ),
-       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
